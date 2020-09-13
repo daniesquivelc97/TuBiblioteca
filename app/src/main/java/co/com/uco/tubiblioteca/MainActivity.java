cfecfc6f31b7222132c3d2243c679c6ba8af27e8
daniesquivelc97 <dani_esquivelc@hotmail.com>
2020-09-12 21:11:08 -0500

    Avances de login y barra de navegación

--- app/src/main/java/co/com/uco/tubiblioteca/MainActivity.java
+++ app/src/main/java/co/com/uco/tubiblioteca/MainActivity.java
@@ -1,8 +1,13 @@
 package co.com.uco.tubiblioteca;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new HomePage()).commit();
+        }
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment fragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            fragment = new HomePage();
+                            break;
+                        case R.id.nav_create:
+                            fragment = new CreateBookPage();
+                            break;
+                        case R.id.nav_list:
+                            fragment = new ListBookPage();
+                            break;
+                        case R.id.nav_profile:
+                            fragment = new UserPage();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment).commit();
+                    return true;
+                }
+            };
 }
