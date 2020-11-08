77295b35867751a2fadb1ebab6992903ec8d61f2
daniesquivelc97 <dani_esquivelc@hotmail.com>
2020-11-07 19:16:33 -0500

    Actualización para crear usuario y login

--- app/src/main/java/co/com/uco/tubiblioteca/LoginActivity.java
+++ app/src/main/java/co/com/uco/tubiblioteca/LoginActivity.java
@@ -11,9 +11,18 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import co.com.uco.tubiblioteca.dao.UsuarioDao;
+import co.com.uco.tubiblioteca.dto.Usuario;
+
 public class LoginActivity extends AppCompatActivity {
+
+    EditText user;
+    EditText password;
+    UsuarioDao usuarioDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +30,37 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         final Button btnLogin = findViewById(R.id.btnLogin);
+        final Button btnRegister = findViewById(R.id.btnRegister);
+
+        user = (EditText) findViewById(R.id.user);
+        password = (EditText) findViewById(R.id.password);
+        usuarioDao = new UsuarioDao(this);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                String usuario = user.getText().toString();
+                String pass = password.getText().toString();
+                if(usuario.equals("") && pass.equals("")){
+                    Toast.makeText(LoginActivity.this, "Error: Campos vacíos", Toast.LENGTH_LONG).show();
+                }
+                else if(usuarioDao.login(usuario,pass) == 1){
+                    Usuario ux = usuarioDao.getUsuario(usuario,pass);
+                    Toast.makeText(LoginActivity.this, "Ingreso autorizado.", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(view.getContext(), MainActivity.class);
+                    intent.putExtra("id", ux.getId());
+                    startActivityForResult(intent, 0);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "El usuario no existe.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), RegisterActivity.class);
                 startActivityForResult(intent, 0);
             }
         });
